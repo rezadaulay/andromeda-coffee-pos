376664b82602a6c0ac292c329a63f892337e8ca7
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2025-12-22 19:19:44 +0700

    feat: setup model fillable and relations

--- app/Models/DetailSale.php
+++ app/Models/DetailSale.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetailSale extends Model
 {
-    //
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'quantity',
+        'price',
+        'subtotal',
+    ];
+
+    public function sale(): BelongsTo
+    {
+        return $this->belongsTo(Sale::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'price',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
+    }
+
+    public function detailSales(): HasMany
+    {
+        return $this->hasMany(DetailSale::class);
+    }
 }
